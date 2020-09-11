966ad9b6367ecfbbe018894e33af5230450116a9
diakite lassana <lassanadiakite90@gmail.com>
2020-09-11 12:17:16 +0200

    après deploiment modif

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,22 +15,7 @@ class DefaultController
                 'id' => 1,
                 'title' => 'test 1',
                 'completed' => false,
-            ],
-            [
-                'id' => 2,
-                'title' => 'test 2',
-                'completed' => false,
-            ],
-            [
-                'id' => 3,
-                'title' => 'test 3',
-                'completed' => false,
-            ],
-            [
-                'id' => 4,
-                'title' => 'test 4',
-                'completed' => false,
-            ],
+            ]
         ], 200, ['Access-Control-Allow-Origin' => '*']);
     }
 }
